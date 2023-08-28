7389d41d4bd6b4a009e534ff417eed7927789cfc
adrielle-b <adriielle@gmail.com>
2023-08-28 13:14:28 -0300

    feat02: cria model User

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+    const user = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+          },
+          displayName: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          image: {
+            type: DataTypes.STRING,
+          }
+    },
+    {
+        timestamps: false,
+        tableName: 'users',
+        underscored: true,
+    });
+  
+    return user;
+  };
